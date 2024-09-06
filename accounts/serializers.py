5ec2bace401a0d17bde6e0fca3f73f84c0bba6a8
qwerrrqw <latiger95@gmail.com>
2024-09-06 19:15:23 +0900

    자기소개 입력 부분 변수명 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,14 +6,14 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'first_name', 'nickname', 'birthday', 'gender', 'bio']  # 입력 필드
+        fields = ['username', 'password', 'email', 'first_name', 'nickname', 'birthday', 'gender', 'PR']  # 입력 필드
         extra_kwargs = {
             'email': {'required': True},  # 이메일 필수 입력
             'first_name': {'required': True},  # 이름 필수 입력
             'nickname': {'required': True},  # 닉네임 필수 입력
             'birthday': {'required': True},  # 생일 필수 입력
             'gender': {'required': False},  # 성별 선택 입력
-            'bio': {'required': False},  # 자기소개 선택 입력
+            'PR': {'required': False},  # 자기소개 선택 입력
         }
 
     # 사용자 생성 시 비밀번호 해싱 처리
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
             nickname=validated_data['nickname'],
             birthday=validated_data['birthday'],
             gender=validated_data.get('gender', ''),  # 선택 입력 필드 (없으면 빈 값으로 처리)
-            bio=validated_data.get('bio', '')  # 선택 입력 필드 (없으면 빈 값으로 처리)
+            PR=validated_data.get('PR', '')  # 선택 입력 필드 (없으면 빈 값으로 처리)
         )
         user.set_password(validated_data['password'])  # 비밀번호 해싱
         user.save()
