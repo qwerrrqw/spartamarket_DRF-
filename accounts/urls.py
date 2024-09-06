4d58f7f3863d45ce89ddd421f4abc94f2735616f
qwerrrqw <latiger95@gmail.com>
2024-09-06 14:53:08 +0900

    회원 CRUD 클레스로 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import(TokenObtainPairView, TokenRefreshView)
 
 appname = "accounts"
 urlpatterns = [
-    path()
+    #
+    path("", views.UserListAPIView, name="article_list"),
+    path("<int:pk>/", views.UserDetailAPIView, name="article_detail"),
+    
+    #
+    
+    path("signin/", TokenObtainPairView.as_view(),  name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(),  name="token_obtain_pair"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
-from .serializers import ArticleSerializer
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+
+        
+class UserListAPIView(APIView):
+    
+    def get(self, request): # 회원 리스트
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request): # 회원 생성
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+class UserDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(User, pk=pk)
+    
+    def get(self, request, pk): # 유저 상세 페이지
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request, pk): # 회원 정보 수정
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        
+    def delete(self, request, pk): # 회원 탈퇴
+        user = self.get_object(pk)
+        user.delete()
+        data = {"delete": f"user({pk}) is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
